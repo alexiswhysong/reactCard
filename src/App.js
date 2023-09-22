2443263164239f3c8831107d34a762bc36ea0cd9
alexiswhysong <142642514+alexiswhysong@users.noreply.github.com>
2023-09-21 22:04:42 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,33 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div class="buttons">
-</div> 
-  <button class="duplicate">Duplicate</button>
-  <button class="modifytitle">Change Name</button>
-  <button id="deletelastcard">Delete</button>
-<button data-toggle-btn>Details</button>
-</div>
-<div class="wrapper">
-  <div class="container">
-  <img class="image" src="https://www.chewboom.com/wp-content/uploads/2022/07/Sheetz-Offers-2-Free-Hot-Dogs-with-Any-Purchase-In-The-App-From-July-20-22-2022.-b.jpg"/>
-  <div class="header">
-    <h3>Sheetz MTO Order</h3>
-    <h4>Hot Dogs</h4>
-  </div>
-  <details class="details">
-    <summary>Food Details</summary>
-    <div>
-      <ul>
-        <li>2 Beef hotdogs on brioche buns with optional codiments such as ketchup, mustard, or chili cheese sauce.</li>
-        <li>additional charges could be applied to MTO</li>
-       </ul>
-    </div>
-    </details>
-      
-</div>
-  
-  
+
+
+    return (
+      <div className="App">
+        <h1>Sheetz MTO Order</h1>
+        <h1>Hot Dogs</h1>
+        <img src="https://www.chewboom.com/wp-content/uploads/2022/07/Sheetz-Offers-2-Free-Hot-Dogs-with-Any-Purchase-In-The-App-From-July-20-22-2022.-b.jpg" alt="Frog" />
+        <div className="paragraph">
+          <p>2 Beef hotdogs on brioche buns with optional codiments such as ketchup, mustard, or chili cheese sauce.</p>
+        </div>
+        <div class="btn-wrapper">
+ <a href="https://hax.psu.edu">
+   <button>Details</button> </a> </div>
+      </div>
+    );
+  }
 
 export default App;
